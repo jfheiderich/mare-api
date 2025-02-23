6ae47f7af8789636bde26807373c51bdbc9b7821
jfheiderich <jf.heiderich@gmail.com>
2025-02-23 18:17:53 -0300

    chore: add cors and helmet

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import router from "./routes";
+import helmet from "helmet";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
-
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
