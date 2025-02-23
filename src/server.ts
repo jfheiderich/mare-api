6e5c5b7bdcfca10596a920031a9a434e0800c7d6
jfheiderich <jf.heiderich@gmail.com>
2025-02-23 18:25:26 -0300

    fix: cors4

--- src/server.ts
+++ src/server.ts
@@ -10,8 +10,8 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: "*",
+    methods: "*",
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
