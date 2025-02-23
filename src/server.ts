4aa24ebc009dfc38bdcf6aee3bea26d957ec7b23
jfheiderich <jf.heiderich@gmail.com>
2025-02-23 18:21:23 -0300

    fix: cors

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,14 @@ dotenv.config();
 
 const app = express();
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
