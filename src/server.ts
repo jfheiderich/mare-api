68190dae7002688e2ba1ef3d268f47416f755ca6
jfheiderich <jf.heiderich@gmail.com>
2025-02-26 20:24:01 -0300

    fix: port cors

--- src/server.ts
+++ src/server.ts
@@ -8,11 +8,13 @@ dotenv.config();
 
 const app = express();
 app.use(helmet());
+
 app.use(
   cors({
-    origin: "*",
-    methods: "*",
+    origin: "http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 
